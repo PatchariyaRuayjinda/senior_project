083df70957e94d7b40edbaeabb935a37f3991eab
Chattrin Rodbouthong <86760606+Domozzz@users.noreply.github.com>
2022-08-30 09:57:45 +0700

    edit findall shelf

--- backend/controllers/productInShelf.js
+++ backend/controllers/productInShelf.js
@@ -1,5 +1,36 @@
 const ProductInShelf = require('../models/productInShelf')
 
+exports.findAllProductInShelf = async(req, res)=>{
+    try{
+        const findInShelf = await ProductInShelf.aggregate([
+            {$lookup:{
+                from: 'products',
+                localField: 'product_id',
+                foreignField: '_id',
+                as: 'product'
+            }},
+            {$lookup: {
+                from: "shelves",
+                localField: "shelf_id",
+                foreignField: "_id",
+                as: "shelf"
+            }},
+            {"$project": {
+                "product.productName": 1,
+                "product.group": 1,
+                "shelf.shelfNumber": 1,
+                "shelf.floorNumber": 1,
+                "shelf.lockNumber": 1
+            }}
+        ])
+        console.log(findInShelf)
+        res.send(findInShelf)
+    }catch(err){
+        console.log(err)
+        res.status(500).send('Server Error!')
+    }
+}
+
 exports.addInShelf = async(req, res) => {
     try {
         // console.log(req.body)
--- backend/routes/productInShelf.js
+++ backend/routes/productInShelf.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {addInShelf} = require('../controllers/productInShelf')
+const {findAllProductInShelf , addInShelf} = require('../controllers/productInShelf')
+
+//@Endpoint http://localhost:3001/api/findAllProductInShelf
+//@method GET
+//@Access Publish
+router.get('/findAllProductInShelf', findAllProductInShelf)
 
 //@Endpoint http://localhost:3001/api/addShelf
 //@method POST
