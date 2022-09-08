441ccaab41a3ee27929ae343a7aca14c6aad9f1e
Seraphic <iwnlyfe@hotmail.com>
2022-09-08 19:47:09 +0700

    export pdf success

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { PDFViewer } from '@react-pdf/renderer';
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Redux
